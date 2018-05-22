af505e8b648e1d223107a367dfc3553ffbed5f18
young <yangxu@yeezon.com>
2018-05-22 22:36:10 +0800

    feat: toat弹出框

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -18,6 +18,24 @@ Page({
             icon: 'success'
         });
     },
+    onShareTap(e) {
+        const itemList = [
+            '分享到微信好友',
+            '分享到朋友圈',
+            '分享到qq',
+            '分享到微博'
+        ];
+        wx.showActionSheet({
+            itemList,
+            itemColor: '#405f80',
+            success(opts) {
+                wx.showModal({
+                    title: `用户${itemList[opts.tapIndex]}`,
+                    content: `无法分享`
+                });
+            }
+        });
+    },
     onLoad(opts) {
         const postId = opts.id;
         const postData = data[postId];
